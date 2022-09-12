feeb0b0152d1cde0d3100f78508dac1612a10308
adarshgajavelli <gajavelliadarsh@gmail.com>
2022-09-12 11:24:38 +0530

    3months update

---
+++ script.js
@@ -0,0 +1,102 @@
+/* let message;
+message = "hello";
+alert(message);
+*/
+
+/* let user = "john", age = 25;
+console.log(user);
+console.log(age);
+age = 35;
+console.log(age);
+alert(age);
+*/
+
+/*let message = "this";
+let messages = "that";
+console.log(message)
+console.log(messages)
+console.log(message + "is" + messages);*/
+
+/* let nameChecked = true;
+console.log(nameChecked)
+let ageChecked = false;
+console.log(ageChecked)
+*/
+
+// no error
+
+/*
+let message = "message";
+message = 123456;
+console.log(message)
+*/
+
+
+// Data Types
+/*
+1. String
+2. Number
+3. Boolean (True, False)
+4. Undefined
+5. Nan
+*/
+
+/* alert("not a number" / 2); */
+
+
+//let year = prompt("What is your age", 0);
+
+/*if (year > 21) alert('you are a major!');
+
+if (year < 21) alert('Be Like a kid!');
+*/
+
+/*if (year > 21) {
+
+    alert('You are major!');
+
+} else {
+
+    alert ("Be like a kid!");
+}*/
+/*
+let year = prompt("What Year Antman Movie release", 0);
+
+if (year < 2015) {
+    alert('Too early.......');
+} else if (year > 2015) {
+    alert("Too late");
+} else {
+    alert('Exactly!');
+}
+*/
+
+/*let a = 2 + 1;
+
+switch (a) {
+    case 3:
+        alert("Too small");
+        break;
+    case 4:
+        alert("Exactly");
+        break;
+    case 5:
+        alert("Too big");
+        break;
+    default:
+        alert("I don't Know such values");
+}*/
+/*
+let i = 5;
+while (i < 10) {
+alert (i);
+i++;
+}
+*/
+
+
+
+
+
+
+
